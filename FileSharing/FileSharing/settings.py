0054352c216d5f32a83093c3acc90175319946c6
Padam Damai <padamdamai888@gmail.com>
2024-09-12 23:10:26 +0545

    model added :Folder and File

--- FileSharing/FileSharing/settings.py
+++ FileSharing/FileSharing/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-luuzrl)%&0+acu!k$flxyi7q1_sa@5e%!l%=t@l21stso_@n_5'
+SECRET_KEY = 'django-insecure-_fzjh846pa+5=xw(d+&pz#02!2ya=iatfi2qggs2vm(50zm6x@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'djangorestframework'
+    'rest_framework',
+    'home'
 ]
 
 MIDDLEWARE = [
